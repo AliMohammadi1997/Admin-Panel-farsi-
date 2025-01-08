71e0de9ffb26483c329442d2802a8e434d1449d7
Ali <mohammadiali658@gmail.com>
2025-01-08 15:22:59 +0330

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
-import { useRoutes } from "react-router-dom"
-import Header from "./Component/Header/Header"
-import Sidebar from "./Component/Sidebar/sidebar"
-import routes from "./routes"
-import { useState } from "react"
+import { useRoutes } from 'react-router-dom'
+import Header from './Component/Header/Header'
+import Sidebar from './Component/Sidebar/sidebar'
+import routes from './routes'
+import { useState } from 'react'
 
 // import './App.css'
 
+function App () {
+  const router = useRoutes(routes)
+  const [isActive, setIsActive] = useState(false)
+  const toggleBtn = () => {
+    setIsActive(!isActive)
+  }
 
-function App() {
+  return (
+    <>
+      <div className='hidden lg:flex w-full '>
+        <Sidebar />
+        <div className='flex-5 m-8 '>
+          <Header />
+          {router}
+        </div>
+      </div>
 
-    const router = useRoutes(routes)
-    const [isActive, setIsActive] = useState(false)
-    const toggleBtn = () => { setIsActive(!isActive) }
+      <div className='flex flex-col lg:hidden w-full '>
+        {isActive && <Sidebar toggleBtn={toggleBtn} isActive={isActive} />}
 
-    return (
-        <>
-            <div className="hidden lg:flex w-full ">
-                <Sidebar />
-                <div className="flex-5 m-8 ">
-                    <Header />
-                    {router}
-                </div>
-            </div>
-
-            <div className="flex flex-col lg:hidden w-full ">
-                {isActive && <Sidebar toggleBtn={toggleBtn} isActive={isActive} />}
-
-                <Header toggleBtn={toggleBtn} />
-                {router}
-
-            </div>
-        </>
-    )
+        <Header toggleBtn={toggleBtn} />
+        {router}
+      </div>
+    </>
+  )
 }
 
 export default App
